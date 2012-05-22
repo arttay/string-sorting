1970a95890362db622113ba926e78b33fc06122e
arttay <arttay1090@yahoo.com>
2012-05-22 15:23:35 -0500

    functiono proototypes for project

---
+++ strarray.h
@@ -0,0 +1,3 @@
+void initStrArray(char* dataArray[], int ARRAYMAX, int STRINGMAX );             //function prototype for initStrArray
+void printArray(char* dataArray[], int ARRAYMAX);                               //function prototype for printArray
+void sortStrArray(char* dataArray[], int ARRAYMAX, int STRINGMAX);              //function prototype for sortStrArray
